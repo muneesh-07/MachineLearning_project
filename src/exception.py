4b07e1fcb984323525416b68c3d9741e5e51a8ab
muneesh-07 <muneeshmeenakshi@gmail.com>
2024-04-28 20:35:12 +0530

    EDA_TRAINING

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,6 @@
 import sys
 import logging
+from src.logger import logging
 
 
 
@@ -22,5 +23,10 @@ class CustomException(Exception):
         return self.error_message
     
 
-
+if __name__ == "__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("zero division Error")
+        raise CustomException(e,sys)
     
